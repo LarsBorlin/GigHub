a5f50af47e5ec248f75419e0bdb480734527e887
Lars Börlin <lars.borlin@gmail.com>
2017-11-20 16:31:19 +0100

    Mine updates

--- GigHub/Controllers/GigsController.cs
+++ GigHub/Controllers/GigsController.cs
@@ -89,7 +89,7 @@ namespace GigHub.Controllers
             _context.Gigs.Add(gig);
             _context.SaveChanges();
 
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Mine", "Gigs");
         }
     }
 }
